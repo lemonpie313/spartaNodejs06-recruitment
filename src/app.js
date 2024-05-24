8762482df39dffcad7866d65393f5673068c42a6
SuhyeonJin <lofi0613@gmail.com>
2024-05-24 11:12:29 +0900

    프로젝트 기본 셋팅 오류해결

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import express from 'express';
-import errorHandlerMiddleware from './middlewares/error-handler.middleware.js';
+import errorHandler from './middlewares/error-handler.middleware.js';
 
 const app = express();
 const PORT = 3500;
 
 app.use(express.json());
-app.use(cookieParser());
 
-
-app.use(errorHandlerMiddleware);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
