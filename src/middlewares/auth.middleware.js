f2c4da44be7463a54bf6a5853344119e1c048e9a
SuhyeonJin <lofi0613@gmail.com>
2024-05-24 17:42:12 +0900

    사용자 인증

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,47 @@
+import jwt from 'jsonwebtoken';
+import { prisma } from '../utils/prisma.util.js';
+import dotEnv from 'dotenv';
+
+dotEnv.config();
+
+export default async function (req, res, next) {
+  try {
+    const { resumeAccessToken } = req.cookies;
+    if (!resumeAccessToken) throw new Error('인증 정보가 없습니다.');
+
+    const [tokenType, token] = resumeAccessToken.split(' ');
+
+    if (tokenType !== 'Bearer')
+      throw new Error('지원하지 않는 인증방식입니다.');
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
+    const userId = decodedToken.id;
+
+    const user = await prisma.users.findFirst({
+      where: { userId: +userId },
+    });
+    if (!user) {
+      res.clearCookie('resumeAccessToken');
+      throw new Error('인증 정보와 일치하는 사용자가 없습니다.');
+    }
+
+    // req.user에 사용자 정보를 저장합니다.
+    req.user = user;
+    next();
+  } catch (error) {
+    res.clearCookie('resumeAccessToken');
+
+    switch (error.name) {
+      case 'TokenExpiredError':
+        return res.status(401).json({ message: '인증 정보가 만료되었습니다.' });
+      case 'JsonWebTokenError':
+        return res
+          .status(401)
+          .json({ message: '인증 정보가 유효하지 않습니다.' });
+      default:
+        return res
+          .status(401)
+          .json({ message: error.message ?? '비정상적인 접근입니다.' });
+    }
+  }
+}
