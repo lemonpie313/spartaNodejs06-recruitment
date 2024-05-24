33a7c0c028b4d6d486c40000cea2176ba130adc6
SuhyeonJin <lofi0613@gmail.com>
2024-05-24 16:05:34 +0900

    회원가입/로그인

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,3 +1,147 @@
 import express from 'express';
-import { prisma } from '../utils/prisma/index.js';
-import { Prisma } from '@prisma/client';
+import dotEnv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import { prisma } from '../utils/prisma.util.js';
+import bcrypt from 'bcrypt';
+//import { Prisma } from '@prisma/client';
+
+dotEnv.config();
+const router = express.Router();
+
+/* 회원가입 */
+router.post('/sign-up', async (req, res, next) => {
+  try {
+    //req.body에서 정보 받아오기
+    const { email, password, passwordConfirm, name } = req.body;
+
+    //정보 기입 오류 시 에러
+    if (!email) {
+      return res.status(400).json({ message: '이메일 주소를 입력해주세요.' });
+    } else if (!password) {
+      return res.status(400).json({ message: '비밀번호를 입력해주세요.' });
+    } else if (!passwordConfirm) {
+      return res.status(400).json({ message: '비밀번호 확인을 입력해주세요.' });
+    } else if (!name) {
+      return res.status(400).json({ message: '이름을 입력해주세요.' });
+    }
+
+    const isExistEmail = await prisma.Users.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (isExistEmail) {
+      return res.status(409).json({ message: '이미 가입된 사용자입니다.' });
+    }
+    const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+    if (!pattern.test(email)) {
+      return res
+        .status(400)
+        .json({ message: '이메일 형태가 올바르지 않습니다.' });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: '비밀번호는 6자리 이상이어야 합니다.' });
+    }
+    if (password != passwordConfirm) {
+      return res.status(400).json({ message: '비밀번호가 일치하지 않습니다' });
+    }
+
+    //비밀번호 암호화
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //데이터 저장
+    const user = await prisma.Users.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+    const userInfo = await prisma.UserInfos.create({
+      data: {
+        userId: user.userId,
+        email,
+        name,
+        password: hashedPassword,
+      },
+      select: {
+        userId: true,
+        email: true,
+        name: true,
+        position: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    //반환
+    return res.status(201).json({
+      status: 201,
+      message: '회원가입이 완료되었습니다.',
+      data: { userInfo },
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/* 로그인 */
+router.post('/sign-in', async (req, res, next) => {
+  try {
+    //req.body에서 정보 받아오기
+    const { email, password } = req.body;
+
+    //정보 기입 오류 에러처리
+    if (!email) {
+      return res.status(400).json({ message: '이메일 주소를 입력해주세요.' });
+    } else if (!password) {
+      return res.status(400).json({ message: '비밀번호를 입력해주세요.' });
+    }
+
+    const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+    if (!pattern.test(email)) {
+      return res.status(400).json({
+        status: 400,
+        message: '이메일 형태가 올바르지 않습니다.',
+      });
+    }
+
+    //해당하는 회원 정보 가져오기
+    const userInfo = await prisma.Users.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (!userInfo) {
+      return res.status(404).json({
+        status: 404,
+        message: '회원 정보를 찾을 수 없습니다.',
+      });
+    }
+    if (!(await bcrypt.compare(password, userInfo.password))) {
+      return res.status(400).json({
+        status: 400,
+        message: '비밀번호가 일치하지 않습니다.',
+      });
+    }
+
+    //accessToken 발급 및 반환
+    const accessToken = jwt.sign(
+      { id: userInfo.userId },
+      process.env.ACCESS_TOKEN_SECRET_KEY,
+      { expiresIn: '12h' },
+    );
+    res.cookie('resumeAccessToken', `Bearer ${accessToken}`);
+
+    //반환
+    res.status(200).json({
+      status: 200,
+      message: '로그인에 완료되었습니다.',
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
