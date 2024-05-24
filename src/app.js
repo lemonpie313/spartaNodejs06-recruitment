5a421fb96f6f679f532f7d006f07b7d61b05c085
SuhyeonJin <lofi0613@gmail.com>
2024-05-24 11:10:04 +0900

    프로젝트 기본 셋팅

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import dotEnv from 'dotenv';
+import errorHandlerMiddleware from './middlewares/error-handler.middleware.js';
 
 const app = express();
 const PORT = 3500;
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
